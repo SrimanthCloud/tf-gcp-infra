cf9f770ef921848d4fc3566e3328320096554caf
Srimanthbabugattamaneni <144981709+Srimanthbabugattamaneni@users.noreply.github.com>
2024-02-22 17:40:48 -0500

    adding instace creation using images

--- variable.tf
+++ variable.tf
@@ -1,24 +1,62 @@
 variable "project_id" {
-  description = "id for the project "
-  default = "linen-totality-412004"
+  description = "ID for the project"
 }
 
 variable "region" {
   description = "The GCP region to deploy resources"
-  default     = "us-east1"
 }
 
 variable "vpc_name" {
   description = "The name of the VPC to create"
-  default    = "srimanth"
 }
 
 variable "webapp_subnet_cidr" {
   description = "CIDR for the webapp subnet"
-  default     = "10.0.1.0/24"
 }
 
 variable "db_subnet_cidr" {
   description = "CIDR for the db subnet"
-  default     = "10.0.2.0/24"
+}
+
+
+variable "vm_name" {
+  description = "The name of the VM instance"
+  type        = string  
+}
+
+variable "vm_zone" {
+  description = "The zone for the VM instance"
+  type        = string
+}
+
+variable "vm_machine_type" {
+  description = "The machine type for the VM instance"
+  type        = string
+}
+
+variable "vm_image" {
+  description = "The custom image for the VM boot disk"
+  type        = string
+}
+
+variable "vm_disk_type" {
+  description = "The disk type for the VM boot disk"
+  type        = string
+}
+
+variable "vm_disk_size_gb" {
+  description = "The size of the VM boot disk in GB"
+  type        = number
+}
+
+variable "vpc_regional" {
+  description = "The size of the VM boot disk in GB"
+  type        = string
+}
+
+
+
+variable "app_port" {
+  description = "The application port to allow through the firewall"
+  type        = string 
 }
