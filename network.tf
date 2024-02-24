abaee88317cb7ac436152792c82e62b95c7ed0d4
Srimanthbabugattamaneni <144981709+Srimanthbabugattamaneni@users.noreply.github.com>
2024-02-23 21:00:40 -0500

    Secondassignment (#3)
    * working code
    
    * adding instace creation using images

--- network.tf
+++ network.tf
@@ -2,7 +2,8 @@ resource "google_compute_network" "vpc" {
   name                    = var.vpc_name
   auto_create_subnetworks = false
   delete_default_routes_on_create = true
-  routing_mode            = "REGIONAL"
+  routing_mode            = var.vpc_regional
+
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
@@ -29,4 +30,52 @@ resource "google_compute_route" "webapp_route" {
   depends_on = [
     google_compute_subnetwork.webapp_subnet
   ]
+}
+
+
+resource "google_compute_firewall" "webapplication" {
+  name    = "${var.vpc_name}-webapplication"
+  network = google_compute_network.vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = [var.app_port]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "deny_ssh" {
+  name    = "${var.vpc_name}-deny-ssh"
+  network = google_compute_network.vpc.id
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name  
+  zone         = var.vm_zone
+  machine_type = var.vm_machine_type
+
+  boot_disk {
+    initialize_params {
+      image = var.vm_image
+      type  = var.vm_disk_type
+      size  = var.vm_disk_size_gb
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc.id
+    subnetwork = google_compute_subnetwork.webapp_subnet.id
+
+    access_config {
+      
+    }
+  }
 }
