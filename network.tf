6a62d68dc4abc627256144de79d712c5fd9e5256
Srimanthbabugattamaneni <144981709+Srimanthbabugattamaneni@users.noreply.github.com>
2024-03-05 15:20:28 -0500

    Sql (#5)
    * working code
    
    * fixing sql error
    
    * Update network.tf

--- network.tf
+++ network.tf
@@ -11,6 +11,7 @@ resource "google_compute_subnetwork" "webapp_subnet" {
   ip_cidr_range = var.webapp_subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc.id
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
@@ -78,4 +79,89 @@ resource "google_compute_instance" "vm_instance" {
       
     }
   }
+
+
+  metadata = {
+    startup-script = "#!/bin/bash\ncat <<EOF > /opt/.env\nDB_HOST=${google_sql_database_instance.cloudsql_instance.private_ip_address}\nDB_NAME=${google_sql_database.webapp_database.name}\nDB_USER=${google_sql_user.webapp_user.name}\nDB_PASSWORD=${random_password.password.result}\nDB_DIALECT=\"mysql\"\nDB_PORT=3306\nEOF\n\nchown csye6225:csye6225 /opt/.env\n"
+  }
+ 
+}
+
+
+
+
+resource "google_project_service" "service_networking" {
+  service = "servicenetworking.googleapis.com"
+  project = var.project_id
+}
+
+
+resource "google_compute_global_address" "private_ip_address" {
+  name          ="google-managed-services-${var.vpc_name}"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 24
+  network       = google_compute_network.vpc.id
+  project       = var.project_id
+}
+
+ resource "google_service_networking_connection" "private_vpc_connection" {
+  network  = google_compute_network.vpc.id
+  service  = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  deletion_policy = "ABANDON"
+  #depends_on             = [google_project_service.service_networking]
+ }
+
+
+resource "google_sql_database_instance" "cloudsql_instance" {
+  provider            = google-beta
+  project             = var.project_id
+  name                = var.Sql_instance_name
+  region              = var.region
+  database_version    = var.database_version
+  deletion_protection = false
+  depends_on          = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    tier              = "db-custom-1-3840"
+    availability_type = var.routing_mode
+    disk_type         = var.sql_disk_type
+    disk_size         = var.db_disk_size
+    disk_autoresize   = true
+
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.vpc.self_link
+      enable_private_path_for_google_cloud_services = true
+    }
+
+    backup_configuration {
+      enabled            = true
+      binary_log_enabled = true
+    }
+  }
+}
+
+resource "google_sql_database" "webapp_database" {
+  name     = var.db_name
+  instance = google_sql_database_instance.cloudsql_instance.name
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "google_sql_user" "webapp_user" {
+  name     = var.db_user
+  instance = google_sql_database_instance.cloudsql_instance.name
+  password = random_password.password.result
+  host     = "%"
+}
+
+output "cloudsql_private_ip" {
+  value = google_sql_database_instance.cloudsql_instance.ip_address
+
 }
--- variable.tf
+++ variable.tf
@@ -1,5 +1,4 @@
 variable "project_id" {
-
   description = "ID for the project"
 }
 
@@ -64,4 +63,78 @@ variable "vpc_regional" {
 variable "app_port" {
   description = "The application port to allow through the firewall"
   type        = string 
+
 }
+
+variable "Sql_instance_name" {
+  description = "The name of the Cloud SQL instance."
+}
+
+
+variable "database_version" {
+  description = "The version of the database for the Cloud SQL instance."
+}
+
+variable "routing_mode" {
+  description = "The routing mode for the Cloud SQL instance availability."
+}
+
+variable "sql_disk_type" {
+  description = "The disk type for the Cloud SQL instance."
+}
+
+
+
+variable "vpc_private_service_access" {
+  description = "The name of the database user."
+}
+
+variable "private_ip_address" {
+  description = "The name of the database user."
+}
+
+variable "forwarding_rule_private_access" {
+  description = "The name of the database user."
+}
+
+variable "private_access_address_type" {
+  description = "The name of the database user."
+}
+
+variable "db_deletion_protection" {
+  description = "The name of the database user."
+}
+
+variable "db_availability_type" {
+  description = "The name of the database user."
+}
+
+variable "db_disk_type" {
+  description = "The name of the database user."
+}
+
+variable "db_disk_size" {
+  description = "The name of the database user."
+}
+
+variable "ipv4_enabled" {
+  description = "The name of the database user."
+}
+
+variable "db_edition" {
+  description = "The name of the database user."
+}
+
+variable "db_tier" {
+  description = "The name of the database user."
+}
+
+variable "db_name" {
+  description = "The name of the database user."
+}
+
+variable "db_user" {
+  description = "The name of the database user."
+}
+
+
